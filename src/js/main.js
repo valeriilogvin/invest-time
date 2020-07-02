9f7c33100c31f389a9f31d40575694f55d51a91e
valeriilogvin <valerii.logvin@gmail.com>
2020-07-02 18:06:14 +0300

    desctop

--- src/js/main.js
+++ src/js/main.js
@@ -72,4 +72,40 @@ $(document).ready(function(){
             // instead of a settings object
         ]
     });
+
+    $('.sect7-slick').slick({
+        infinite: false,
+        slidesToShow: 2,
+        slidesToScroll: 1,
+        dots:true,
+        dotsClass: 'my-slick-dots',
+        prevArrow: '<button type="button" class="my-slick-prev"><svg class="icon icon-slick-l"><use xlink:href="#icon-slick-l"></use></svg></button>',
+        nextArrow: '<button type="button" class="my-slick-next"><svg class="icon icon-slick-r"><use xlink:href="#icon-slick-r"></use></svg></button>',
+        responsive: [
+            {
+                breakpoint: 1024,
+                settings: {
+                    slidesToShow: 3,
+                    slidesToScroll: 3,
+                }
+            },
+            {
+                breakpoint: 600,
+                settings: {
+                    slidesToShow: 1,
+                    slidesToScroll: 1
+                }
+            },
+            {
+                breakpoint: 480,
+                settings: {
+                    slidesToShow: 1,
+                    slidesToScroll: 1
+                }
+            }
+            // You can unslick at a given breakpoint now by adding:
+            // settings: "unslick"
+            // instead of a settings object
+        ]
+    });
 });
